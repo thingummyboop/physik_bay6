9c0cde87af62ff530bff81d72eee513febb0ced8
Simon <simon@example.com>
2026-04-30 07:00:43 +0200

    Align Rechnung Werkstatt workbench

--- js/renderer.js
+++ js/renderer.js
@@ -126,7 +126,7 @@ async function renderTopic() {
 
         // Load Script
         const script = document.createElement('script');
-        script.src = `../js/topics/${topicId}.js?v=6.9`;
+        script.src = `../js/topics/${topicId}.js?v=7.0`;
         script.async = false;
         script.onload = () => {
             if (typeof topicInit === 'function') {
