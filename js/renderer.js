b52d1a4ae8f5e3f00bb774d4c83d9d3b7c716914
Simon <simon@example.com>
2026-05-26 02:33:16 +0200

    Fix dumbbell bar orientation

--- js/renderer.js
+++ js/renderer.js
@@ -134,13 +134,13 @@ async function renderTopic() {
 
     try {
         // Fetch language data (added cache busting)
-        let response = await fetch(`../lang/${lang}.json?v=6.8`);
+        let response = await fetch(`../lang/${lang}.json?v=6.9`);
         let langData = await response.json();
         let topic = langData[topicId];
         let germanTopic = null;
 
         if (lang !== 'de') {
-            const deRes = await fetch(`../lang/de.json?v=6.8`);
+            const deRes = await fetch(`../lang/de.json?v=6.9`);
             const deData = await deRes.json();
             germanTopic = deData[topicId];
         }
