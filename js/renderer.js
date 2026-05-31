34d87aa8e8f31bb76c8d5dd35a7b2357da526eaf
Simon <simon@example.com>
2026-05-31 15:42:31 +0200

    Add sixth-grade biology curriculum

--- js/renderer.js
+++ js/renderer.js
@@ -540,13 +540,13 @@ async function renderTopic() {
 
     try {
         // Fetch language data (added cache busting)
-        let response = await fetch(`../lang/${lang}.json?v=7.5`);
+        let response = await fetch(`../lang/${lang}.json?v=7.6`);
         let langData = await response.json();
         let topic = langData[topicId];
         let germanTopic = null;
 
         if (lang !== 'de') {
-            const deRes = await fetch(`../lang/de.json?v=7.5`);
+            const deRes = await fetch(`../lang/de.json?v=7.6`);
             const deData = await deRes.json();
             germanTopic = deData[topicId];
         }
