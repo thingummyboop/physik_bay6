74828f5d9d5082d07725324b1384e3a5b7612123
Simon <simon@example.com>
2026-06-02 15:35:17 +0200

    Update astronomy shooting star prompt

--- js/renderer.js
+++ js/renderer.js
@@ -1375,13 +1375,13 @@ async function renderTopic() {
 
     try {
         // Fetch language data (added cache busting)
-        let response = await fetch(`../lang/${lang}.json?v=9.6`);
+        let response = await fetch(`../lang/${lang}.json?v=9.9`);
         let langData = await response.json();
         let topic = langData[topicId];
         let germanTopic = null;
 
         if (lang !== 'de') {
-            const deRes = await fetch(`../lang/de.json?v=9.8`);
+            const deRes = await fetch(`../lang/de.json?v=9.9`);
             const deData = await deRes.json();
             germanTopic = deData[topicId];
         }
