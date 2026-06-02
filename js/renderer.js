a64680accbb7904ecd27b93182f170c6fbff4f91
Simon <simon@example.com>
2026-06-02 14:38:06 +0200

    Add astronomy practice questions

--- js/renderer.js
+++ js/renderer.js
@@ -1313,13 +1313,13 @@ async function renderTopic() {
 
     try {
         // Fetch language data (added cache busting)
-        let response = await fetch(`../lang/${lang}.json?v=9.4`);
+        let response = await fetch(`../lang/${lang}.json?v=9.5`);
         let langData = await response.json();
         let topic = langData[topicId];
         let germanTopic = null;
 
         if (lang !== 'de') {
-            const deRes = await fetch(`../lang/de.json?v=9.4`);
+            const deRes = await fetch(`../lang/de.json?v=9.5`);
             const deData = await deRes.json();
             germanTopic = deData[topicId];
         }
